c38b15a6e8d12196912bd70dcbb9ad73c7fd04f0
Paul André Johne (inf1984) <paul_andre.johne@smail.th-koeln.de>
2021-08-22 14:10:19 +0200

    added unusable water_fragment.glsl for textureblending

---
+++ project/assets/shaders/water_fragment.glsl
@@ -0,0 +1,28 @@
+#version 330 core
+
+/* NOTE: Usage shown in video, but unused in main due to implementability */
+
+// NO "in vec3 col" for "out vec4 color", since we're only using textures
+uniform sampler2D diffPalette;
+
+uniform int shaderType;
+
+out vec4 color;
+
+in struct VertexData {
+    vec3 position;
+    vec2 textureCoordinates;
+    vec3 normal;
+} vertexData;
+
+void main() {
+    vec3 diffPaletteTerm = texture(diffPalette, vertexData.textureCoordinates).rgb;
+
+    color += vec4(diffPaletteTerm, 0.75f);
+
+    vec3 lum = vec3(0.299f, 0.587f, 0.114f);
+
+    if(shaderType == 1){
+        color = vec4(vec3(dot(color.rgb, lum)), color.a);
+    }
+}
